a667ba9ae7cffcd52c59502b63f4c0b99e5b4aec
Yoandry Lumpuy <yoandry.lumpuy@sendvalu.com>
2023-11-16 03:22:32 -0500

    Seed data routine invoked in Program class

--- Infrastructure.Startup.Project/Program.cs
+++ Infrastructure.Startup.Project/Program.cs
@@ -1,4 +1,5 @@
 using Infrastructure;
+using Infrastructure.Persistence.Seed;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -9,5 +10,7 @@ builder.Services.AddControllers();
 builder.Services.AddSqlServerContext(builder.Configuration);
 
 var app = builder.Build();
+
+await DbContextSeed.SeedDataBase(app.Services);
 
 app.Run();
